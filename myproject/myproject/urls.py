893cc47be2d3d07839bb79c8836850e9cf9c12f3
Mark Justine DIokno <22-49017@g.batstate-u.edu.ph>
2025-11-04 07:23:05 +0800

    Fix migration route import

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.core.management import call_command
 
 def run_migrations(request):
     call_command('migrate')
