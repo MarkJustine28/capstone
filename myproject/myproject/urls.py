e1b7168845b82a82ca2c8c027e58d151d94d88a1
Mark Justine DIokno <22-49017@g.batstate-u.edu.ph>
2025-11-04 07:42:43 +0800

    Add temporary create-admin endpoint

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -2,15 +2,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 
 def run_migrations(request):
     call_command('migrate')
     return HttpResponse("✅ Migrations applied successfully!")
 
+def create_admin(request):
+    User = get_user_model()
+    username = "admin"
+    email = "admin@example.com"
+    password = "admin123"
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        return HttpResponse("✅ Superuser created successfully!")
+    else:
+        return HttpResponse("⚠️ Superuser already exists.")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path("run-migrations/", run_migrations),
+    path("create-admin/", create_admin),
 
     # Root endpoint (for testing)
     path('', lambda request: HttpResponse("🚀 Guidance Tracker API is running")),
