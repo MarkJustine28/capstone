95971e20dae7c8fcc52b93c5f9cc2657f1c7d7fe
Mark Justine DIokno <22-49017@g.batstate-u.edu.ph>
2025-11-05 15:39:45 +0800

    Fix reports AppConfig reference (ReportConfig instead of ReportsConfig)

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reports.apps.ReportsConfig',
+    'reports.apps.ReportConfig',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
