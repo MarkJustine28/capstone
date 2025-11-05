55167feed396742efc6b3e41a9290e7eabd62244
Mark Justine DIokno <22-49017@g.batstate-u.edu.ph>
2025-11-05 15:35:58 +0800

    Fix AppConfig name mismatch in reports app

--- myproject/reports/apps.py
+++ myproject/reports/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class ReportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'report'
+    name = 'reports'
 
     def ready(self):
         import report.signals  # 👈 Ensures signals are registered
