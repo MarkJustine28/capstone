4a70ec6be6f0105e33abc2704b82f071bcf48022
Mark Justine DIokno <22-49017@g.batstate-u.edu.ph>
2025-11-15 14:35:42 +0800

    fix depolyment error

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'reports',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
